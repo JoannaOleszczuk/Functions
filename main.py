a5da9fa60bb9e54475fb0e61a48ae410b22c9ef6
JOANNA OLESZCZUK <jooleszczuk@wp.pl>
2022-12-30 19:52:57 +0100

    add factorial function

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ def add2(c):
     for i in c:
         tmpSum += i
     return tmpSum
-print(add2([1,2,3,4,5]))
+
+
+print(add2([1, 2, 3, 4, 5]))
+
 
 def multiply2(d):
     tmpMul = 1
     for i in d:
         tmpMul = tmpMul * i
     return tmpMul
-print(multiply2([1,2,3,4,5]))
+
+
+print(multiply2([1, 2, 3, 4, 5]))
+
 
 def add(a, b):
     return a + b
@@ -40,5 +46,20 @@ print(multiply(2, 5))
 
 print(add(substract(2, 3), multiply(4, 5)))
 
+print("Factorial")
 
+# n! = (n-1)*n*(n+1)*...*(n+i), n,i=int
+# n*(n-1)!, n>1
+def factorial(n):
+    if n<0:
+        return None
+    elif n>1:
+        return n*factorial(n-1)
+    elif n==1:
+        return 1
+    elif n==0:
+        return 0
 
+print(factorial(10))
+
+# factorial(3) = 3*factorial(2)=3*2*factorial(1)
